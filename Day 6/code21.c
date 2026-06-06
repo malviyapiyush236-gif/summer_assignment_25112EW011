8a800a35e51400ae7bbab68f3f5101ad3e091928
malviyapiyush236-gif <malviyapiyush236@gmail.com>
2026-06-06 22:15:15 +0530

    Add files via upload

---
+++ Day 6/code21.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+    int num, binary[32], i = 0;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    // Convert decimal to binary
+    while (num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    // Print binary number (in reverse order)
+    printf("Binary equivalent: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+
+    return 0;
+}
