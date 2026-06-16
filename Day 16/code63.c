f42fcd8c22ddd3e4631da48028554326e7a74db9
malviyapiyush236-gif <malviyapiyush236@gmail.com>
2026-06-16 20:32:31 +0530

    Add files via upload

---
+++ Day 16/code63.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+    int arr[] = {8, 7, 2, 5, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    int found = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == target) {
+                printf("Pair found: (%d, %d)\n", arr[i], arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if(found)
+            break;
+    }
+
+    if(!found)
+        printf("No pair found.\n");
+
+    return 0;
+}
